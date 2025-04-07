6a77d6e6fa4dd916e9f6e3c57d76cbfae87539ae
Himaanshii <himanshi2157.be21@chitkara.edu.in>
2025-04-07 10:50:47 +0530

    [Himanshi] add. Parse Json and filter only those records where age > 25

---
+++ JSON_Data/src/main/java/Practice_Problems/FilterJSON/filterjson.java
@@ -0,0 +1,36 @@
+package Practice_Problems.FilterJSON;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class filterjson {
+    public static void main(String[] args)throws Exception {
+        String path = new String(
+                Files.readAllBytes(Paths.get("D:\\Capgemini\\Week5\\src\\JSON_Data\\src\\main\\java\\Practice_Problems\\FilterJSON\\Data.json"))
+        );
+
+
+        JSONObject obj = new JSONObject(path);
+
+        JSONArray users = obj.getJSONArray("users");
+
+        JSONArray filtered = new JSONArray();
+
+        for (int i = 0; i < users.length(); i++) {
+            JSONObject user = users.getJSONObject(i);
+            int age = user.getInt("age");
+
+            if (age > 25) {
+                filtered.put(user);
+            }
+        }
+
+        JSONObject result = new JSONObject();
+        result.put("users_under_25", filtered);
+
+        System.out.println(result.toString(2));
+    }
+}
